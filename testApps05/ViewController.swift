99b7e6c359e87ac12d1d6889fd94965180a142c5
Hiroshi Tone <valkyrie.gtr@gmail.com>
2018-12-12 01:42:02 +0900

    Initial Commit

---
+++ testApps05/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApps05
+//
+//  Created by 遠野ひろし on 2018/12/12.
+//  Copyright © 2018 遠野工房. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
